dc9536083c959667eeea5654272e6f4fc8a521f5
POULAT Axel <axel.poulat1@gmail.com>
2019-04-02 16:32:44 +0200

    add commentary

--- src/hand.h
+++ src/hand.h
@@ -6,36 +6,59 @@
 #include <QWidget>
 #include <QTimer>
 
+
+//Class gerant la detection des mains et l'envois de signals 
+//lorsque l'utilisateur effectue des actions
+//Auteur : Axel POULAT
 class Hand : public QObject
 {
         Q_OBJECT
 private:
+//Le Haarcascade pour detecter les mains
     cv::CascadeClassifier cascade_;
+	
+//stoque tout les "mains" trouvées par le haar
     std::vector<cv::Rect> hands_;
+	
+//la vraie main gauche/droite
     cv::Rect leftHand_;
     cv::Rect rightHand_;
+	
+//les timer d'absence des mains permettent de sauvegarder la position 
+//des mains pendant un temps fixe lorsque elles ne sont plus detectées
+//cela rend la detection plus robuste au prix d'un temps de réaction
+//les booleans sont la pour acter de la presence ou non des mains
     QTimer timerOfRightHandPresence_;
     bool isRightHand = false;
     QTimer timerOfLeftHandPresence_;
     bool isLeftHand = false;
 
+//Slot permettant de metre les booleans a faux lorsque 
+//les timers sont expirées
  private slots:
     void leftHandDisapear();
     void rightHandDisapear();
 
 public:
+	//constructeur
     Hand();
+	//load le fichier Haarcascade
     bool loadCascade(const std::string path);
+	//detecte toutes les "mains" sur l'images
     void detectHand(cv::Mat& frame);
+	//identifie la vraie main gauche/droite
     void identifyHand();
+	//dessine les carrés de detections des "mains"
     void drawAllBoundingBox(cv::Mat& frame, const cv::Scalar color = cv::Scalar(0,255,0), const std::string text = "HAND") const;
-    void drawLeftHand(cv::Mat& frame, const cv::Scalar color = cv::Scalar(0,255,0), const std::string text = "LEFT HAND") const;
+    //dessine les carrés de detections des vraie mains
+	void drawLeftHand(cv::Mat& frame, const cv::Scalar color = cv::Scalar(0,255,0), const std::string text = "LEFT HAND") const;
     void drawRightHand(cv::Mat& frame, const cv::Scalar color = cv::Scalar(0,255,0), const std::string text = "RIGHT HAND") const;
-    void detectRotateByHand();
-    void detectStrafeByHand();
-    void detectFastFallByHand(int seuilY);
+    //detecte les commandes utilisateur
+	void detectRotateByHand(); //rotation
+    void detectStrafeByHand(); //translation droite/gauche
+    void detectFastFallByHand(int seuilY); //descente rapide
 
-
+//les signaux emis lors des commandes utilisateurs
 signals :
     void rotate();
     void strafeLeft();
