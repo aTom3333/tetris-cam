26ca655f7c16c1eba6a74fd84f4009d2bcf43552
POULAT Axel <axel.poulat1@gmail.com>
2019-04-04 09:46:17 +0200

    add commentary

--- src/Tetris/Tetris.hpp
+++ src/Tetris/Tetris.hpp
@@ -8,33 +8,47 @@
 
 namespace Tetris
 {
-
+    //Class gérant le jeu et faisant le lien entre
+    //affichage, grille logique et detection des mains
+    //Auteur : Thomas FERRAND
     class Tetris
     {
     public:
+        //constructeur
         Tetris();
         
+        //methodes pour déplacer le tetrominos
         bool goLeft();
         bool goRight();
         bool goDown();
         
         void fallDown();
         
+        //acesseur
         Grid const& getGrid() const noexcept { return grid; }
         uint32_t getScore() const noexcept { return score; }
         
+        //verifie la fin de partie
         bool testDefeat() const;
         
     private:
+        //transforme le tetrominos courant en block dans la grille au contact du "sol"
         void turnIntoBlock();
+        //efface les lignes entieres de la gille
         void clearCompletedLines();
+        //fait apparaitre le prochain tetrominos courant
         void spawnNew();
         
     private:
+        //grille logique
         Grid grid;
+        //class gérant les prochain tetrominos
         Bag bag;
+        //tetrominos courant
         std::unique_ptr<Tetromino> current;
+        //score de la partie
         uint32_t score = 0;
+        //id du tetrominos géneré
         uint32_t currentId = 1;
     };
 
